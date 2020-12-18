c0310b5eef88ea6b10a37cd2db8f81dbdbe04fa1
Resheil Agarwal <resheil@resheil.com>
2020-12-19 03:36:36 +0530

    Connection file

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+//$username = "portalw_stoneown";
+//$password = "6w@eW6s&@g~m";
+//$dbname = "portalw_stonemart";
+$username = "root";
+$password = "";
+$dbname = "granimart";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
---
+++ website_creator/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "portalw_stoneown";
+$password = "6w@eW6s&@g~m";
+$dbname = "portalw_stonemart";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
